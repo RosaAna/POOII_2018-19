5d7dc090132c8b4122b55e7b6a5bc04931d96754
manuel <manolo@iesvirgendelcarmen.com>
2019-02-15 12:46:01 +0100

    Commit final

--- src/com/iesvirgendelcarmen/ejercicios/ejercicio2/CreditCard.java
+++ src/com/iesvirgendelcarmen/ejercicios/ejercicio2/CreditCard.java
@@ -4,11 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class CreditCard {
-	
-	private String numberCreditCard;
+	private static final int NUMBER_OF_DIGIT = 16;
+	private String numberCreditCard;  //16 digits
 	private String owner;
 	private LocalDate creationDate = LocalDate.now();
-	
+
 	public CreditCard(String numberCreditCard, String owner) {
 		this.numberCreditCard = numberCreditCard;
 		this.owner = owner;
@@ -33,10 +33,10 @@ public class CreditCard {
 	public LocalDate getCreationDate() {
 		return creationDate;
 	}
-	
-	
-	
-	
+
+
+
+
 	@Override
 	public String toString() {
 		return String.format("%S%n%s%nExpiration date: %d/%s%n", owner, numberCreditCard, 
@@ -46,18 +46,58 @@ public class CreditCard {
 
 
 	public static boolean checkCreditNumber(String creditNumberToValidate) {
-		
-		return false;
+		if (! creditNumberToValidate.matches("[0-9]{" + NUMBER_OF_DIGIT + "}"))
+			return false;
+		class DigitControl{
+			private String creditNumberWithoutDigitControl;
+
+			public DigitControl(String creditNumberWithoutDigitControl) {
+				this.creditNumberWithoutDigitControl = creditNumberWithoutDigitControl;
+			}
+			public String generateCreditCardWithDigitControl() {
+				int digitCrontol = generateDigitControl();
+				return creditNumberWithoutDigitControl + digitCrontol;
+			}
+			private int generateDigitControl() {
+				StringBuilder sBuilder = new StringBuilder(creditNumberWithoutDigitControl);
+				String creditNWDCReverse = sBuilder.reverse().toString();
+
+				int sum = 0,  partialValue = 0;
+				for (int i = 0; i < creditNWDCReverse.length(); i ++) { 
+					if ( i % 2 == 0) {
+						partialValue = (creditNWDCReverse.charAt(i) - 48) * 2;
+						if (partialValue > 9 ) {
+							partialValue %= 10;
+							partialValue++;
+						}
+					}
+
+					else
+						partialValue = creditNWDCReverse.charAt(i) - 48;
+
+					sum += partialValue;
+				}
+
+
+
+				return sum % 10;
+			}
+		}
+		DigitControl dcControl = new DigitControl(
+				creditNumberToValidate.substring(0, NUMBER_OF_DIGIT - 1));
+		return creditNumberToValidate.equals(
+				dcControl.generateCreditCardWithDigitControl());
 	}
-	
-	
+
+
 	public static void main(String[] args) {
-		CreditCard card = new CreditCard("1111", "pepe");
+		CreditCard card = new CreditCard("2044750038249274", "pepe");
 		System.out.println(card);
-		
+		System.out.println("Tarjeta es válida: " +
+				CreditCard.checkCreditNumber(card.getNumberCreditCard()));
 		
 	}
 
-	
-	
+
+
 }
